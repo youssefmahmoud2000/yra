5eb22afe6e601dca8dbd90fcde716063e0018759
youssef mahmoud <youssefmahmoud462000@gmail.com>
2022-11-08 21:26:03 +0200

    ii

---
+++ index.js
@@ -0,0 +1,34 @@
+// if (new Date() .getHours() >18) {
+//     document.getElementById("demo").innerHTML= "good day";
+// } else {
+//     document.getElementById("demo").innerHTML= "good evening";
+// }
+
+// var number1 = 1000;
+// var number2 =2000;
+// console.log(number1 + number2);
+// function dodo()  {
+// alert("hello mother fucker") 
+// console.log("hellow")
+// document.getElementById("demo").innerHTML= "ya m3rs";
+// }
+// dodo();
+// var num1 = 2000;
+// var num2 = 3000;
+// console.log(num1 + num2);
+// // document.write(num1+num2);
+// document.getElementById("hh").innerHTML = num1 + num2;
+// function navlist() {
+//   var div= document.getElementById("myDiv");
+//   if (div.style.display==="none") {
+//     div.style.display="flex"
+//   } else { 
+
+//      div.style.display="none"
+//   }
+// }
+// function toggle_color() {
+//   var x = document.body;
+//   x.classList.toggle("light_mode")
+  
+// }
